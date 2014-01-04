65bf6e906559dfecc1f2551aa25fbb9e776ab085
Karthik Lakshmanan <karthik.gokarts@gmail.com>
2014-01-04 10:16:43 +0530

    Basic Landing page v1

--- Android/src/com/gchack/infone/MainActivity.java
+++ Android/src/com/gchack/infone/MainActivity.java
@@ -1,16 +1,47 @@
 package com.gchack.infone;
 
-import android.os.Bundle;
+import java.security.MessageDigest;
+import java.security.NoSuchAlgorithmException;
+
 import android.app.Activity;
+import android.os.Bundle;
+import android.util.Base64;
+import android.util.Log;
 import android.view.Menu;
+import android.view.View;
+
+import com.facebook.*;
+import com.facebook.model.*;
+
+import android.widget.Button;
+import android.widget.TextView;
+import android.content.Intent;
+import android.content.pm.PackageInfo;
+import android.content.pm.PackageManager;
+import android.content.pm.PackageManager.NameNotFoundException;
+import android.content.pm.Signature;
+
 
 public class MainActivity extends Activity {
 
+
+	
 	@Override
 	protected void onCreate(Bundle savedInstanceState) {
-		super.onCreate(savedInstanceState);
-		setContentView(R.layout.activity_main);
-	}
+	    super.onCreate(savedInstanceState);
+		setContentView(R.layout.landingpage);
+		Button button = (Button) findViewById(R.id.login_button);
+		//startActivity(new Intent(getBaseContext(), LoginScreen.class));
+		
+		button.setOnClickListener(new View.OnClickListener() {
+            public void onClick(View v) {
+        		// launch page
+            	//startActivity(new Intent(getBaseContext(), LoginScreen.class));
+        	    };                       
+	});
+		
+	  }
+	
 
 	@Override
 	public boolean onCreateOptionsMenu(Menu menu) {
@@ -18,5 +49,12 @@ public class MainActivity extends Activity {
 		getMenuInflater().inflate(R.menu.main, menu);
 		return true;
 	}
-
+	
+	
 }
+	
+	
+
+
+
+
